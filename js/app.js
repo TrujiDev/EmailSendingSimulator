bdfa021784c96a96bb2d4ae07e1923cb11339fb1
Eduar Trujillo <eetgrisales@gmail.com>
2023-10-19 13:05:26 -0500

    Creation of events for the different fields, validation of the information entered in the fields and generation of an error in case one of the fields is empty.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const inputEmail = document.querySelector('#email');
-    const inputSubject = document.querySelector('#subject');
-    const inputMessage = document.querySelector('#message');
+  const inputEmail = document.querySelector("#email");
+  const inputSubject = document.querySelector("#subject");
+  const inputMessage = document.querySelector("#message");
+
+  inputEmail.addEventListener("blur", validate);
+  inputSubject.addEventListener("blur", validate);
+  inputMessage.addEventListener("blur", validate);
+
+  function validate(event) {
+    if (event.target.value.trim() === "") {
+      showError();
+    } else {
+      console.log("Not empty");
+    }
+  }
+
+  function showError() {
+    const error = document.createElement("P");
+    error.textContent = "There was an error";
+
+    console.log(error);
+  }
 });
