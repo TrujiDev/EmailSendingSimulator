d20e4548777771da491bef3b3e3e7d854dfaa80b
Eduar Trujillo <eetgrisales@gmail.com>
2023-10-19 10:59:51 -0500

    Creation of variables and selection of DOM elements

---
+++ js/app.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const inputEmail = document.querySelector('#email');
+    const inputSubject = document.querySelector('#subject');
+    const inputMessage = document.querySelector('#message');
+});
