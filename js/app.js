eb4f518d065b4bd64659572cea1b4d48e2a3cf56
Eduar Trujillo <eetgrisales@gmail.com>
2023-10-21 13:59:25 -0500

    Added form submission button validation

--- js/app.js
+++ js/app.js
@@ -2,23 +2,38 @@ document.addEventListener("DOMContentLoaded", function () {
   const inputEmail = document.querySelector("#email");
   const inputSubject = document.querySelector("#subject");
   const inputMessage = document.querySelector("#message");
+  const btnSubmit = document.querySelector('#form button[type="submit"]');
 
-  inputEmail.addEventListener("blur", validate);
-  inputSubject.addEventListener("blur", validate);
-  inputMessage.addEventListener("blur", validate);
+  const email = {
+    email: '',
+    subject: '',
+    message: ''
+  }
+
+  inputEmail.addEventListener("input", validate);
+  inputSubject.addEventListener("input", validate);
+  inputMessage.addEventListener("input", validate);
 
   function validate(event) {
     if (event.target.value.trim() === "") {
       showError(`The ${event.target.id} field cannot be empty`, event.target.parentElement);
+      email[event.target.name] = '';
+      checkEmail();
       return;
     }
 
     if (event.target.id === "email" && !emailValidate(event.target.value)) {
       showError("The email is invalid", event.target.parentElement);
+      email[event.target.name] = '';
+      checkEmail();
       return;
     }
 
     cleanAlert(event.target.parentElement);
+
+    email[event.target.name] = event.target.value.trim().toLowerCase();
+
+    checkEmail();
   }
 
   function showError(msg, ref) {
@@ -42,5 +57,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
     const result = regex.test(email);
     return result;
+  }
+
+  function checkEmail() {
+    if (Object.values(email).includes('')) {
+      btnSubmit.classList.add('opacity-50');
+      btnSubmit.disabled = true;
+      return;
+    }
+    btnSubmit.classList.remove('opacity-50');
+    btnSubmit.disabled = false;
   }
 });
